b80b0de5b7985d96dd304620d45405ef8843c238
tomipaul <tomipaul95@gmail.com>
2026-02-24 03:19:29 -0500

    Create static folder for collection of static files

--- backend/harvest/settings/prod.py
+++ backend/harvest/settings/prod.py
@@ -4,6 +4,5 @@ DEBUG = False
 ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOST", "")]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Production-specific settings can go here
