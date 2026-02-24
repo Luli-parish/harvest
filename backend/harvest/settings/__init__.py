612b92e8cfa78882e50c16b7ccc42e8e969e8a61
tomipaul <tomipaul95@gmail.com>
2026-02-24 02:03:31 -0500

    Update settings and install gunicorn

--- backend/harvest/settings/__init__.py
+++ backend/harvest/settings/__init__.py
@@ -1,1 +1,9 @@
 # This file marks the settings directory as a Python package.
+# Use one of the following in your DJANGO_SETTINGS_MODULE:
+#   harvest.settings.dev  (for development)
+#   harvest.settings.prod (for production)
+#
+# Example usage:
+#   python manage.py runserver --settings=harvest.settings.dev
+#   python manage.py runserver --settings=harvest.settings.prod
+#
