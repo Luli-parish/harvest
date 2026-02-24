144b12b587c06437751045542dcb52fe377967d4
tomipaul <tomipaul95@gmail.com>
2026-02-24 02:51:27 -0500

    Collect and serve static files

--- backend/harvest/settings/prod.py
+++ backend/harvest/settings/prod.py
@@ -3,4 +3,7 @@ from .base import *
 DEBUG = False
 ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOST", "")]
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 # Production-specific settings can go here
