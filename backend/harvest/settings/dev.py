ba102c6c69e82cf98d22897f440926446d1554d7
tomipaul <tomipaul95@gmail.com>
2026-02-26 18:34:49 -0500

    Update settings

--- backend/harvest/settings/dev.py
+++ backend/harvest/settings/dev.py
@@ -3,4 +3,9 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = ['localhost']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
+
+# CORS settings
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
 # Development-specific settings can go here
