34bf4008afc4cf9ab3c142ad0a0fbf158c0572e5
tomipaul <tomipaul95@gmail.com>
2026-02-27 23:23:49 -0500

    Add HarvestPayment to admin

--- backend/juvenileharvest/admin.py
+++ backend/juvenileharvest/admin.py
@@ -1,4 +1,9 @@
 import pghistory.admin
 from django.contrib import admin
 
+from .models import HarvestPayment
+
 # Register your models here.
+@admin.register(HarvestPayment)
+class HarvestPaymentAdmin(admin.ModelAdmin):
+	list_display = ('id', 'family', '__str__')
