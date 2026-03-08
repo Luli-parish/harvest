f3417711309b2387e3f198176a1469fa46fa1ef8
tomipaul <tomipaul95@gmail.com>
2026-03-08 00:33:19 -0500

    Register Family model in Admin

--- backend/juvenileharvest/admin.py
+++ backend/juvenileharvest/admin.py
@@ -2,9 +2,11 @@ import pghistory.admin
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import AuthUser, HarvestPayment
+from .models import AuthUser, HarvestPayment, Family
 
 # Register your models here.
+admin.site.register(Family)
+
 @admin.register(HarvestPayment)
 class HarvestPaymentAdmin(admin.ModelAdmin):
 	list_display = ('id', 'family', '__str__')
